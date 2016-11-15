c302077b0c1f4db9ef5e02edc5017aabe8ab6f07
Claire Sial and Ethan Law <ethan@>
2016-11-15 15:58:54 -0800

    add hover description to animals, and animation to images

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,23 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-$(document).ready(function(){
 
+function animationHover(element, animation){
+  element = $(element);
+  element.hover(
+    function() {
+      element.addClass('animated ' + animation);
+    },
+    function(){
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+        element.removeClass('animated ' + animation);
+      }, 2000);
+    });
+  }
+
+$(document).ready(function(){
+  $('.animal-picture').each(function() {
+        animationHover(this, 'bounce');
+    });
 });
 
 },{}]},{},[1]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
 
+function animationHover(element, animation){
+  element = $(element);
+  element.hover(
+    function() {
+      element.addClass('animated ' + animation);
+    },
+    function(){
+      //wait for animation to finish before removing classes
+      window.setTimeout( function(){
+        element.removeClass('animated ' + animation);
+      }, 2000);
+    });
+  }
+
+$(document).ready(function(){
+  $('.animal-picture').each(function() {
+        animationHover(this, 'bounce');
+    });
 });
